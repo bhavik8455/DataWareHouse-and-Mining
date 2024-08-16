c476df18061f82d733d5b5c5180b774c0a8bb6a8
Bhavik  Solanki <141156721+bhavik8455@users.noreply.github.com>
2024-08-16 23:27:33 +0530

    Prediction using Naive Bayers Classifier Algorithm

--- main.py
+++ main.py
@@ -21,26 +21,24 @@ with open('data.csv', 'w') as csv_file:
 
     writer.writerows(data)
 
-# Color =Red TYPE =SUV ORIGIN =Domestic STOLEN =Yes
+#Initilize  Mendatory Variable
+ProbRed= len([row for row in data if row[1] == 'Red'])/len(data)
+ProbYes= len([row for row in data if row[4]=='Yes'])/len(data)
+ProbSuv = len([row for row in data if row[2]=='Suv'])/len(data)
+ProbDomes = len([row for row in data if row[3]=='Domestic'])/len(data) 
+
 
 #Calculated Probability of Stolen=Yes and Color=Red
 ProbYesRed = len([row for row in data if row[1] == 'Red' and row[4] == 'Yes'])/len([row for row in data if row[1]=='Red'])
-ProbRed= len([row for row in data if row[1] == 'Red'])/len(data)
-ProbYes= len([row for row in data if row[4]=='Yes'])/len(data)
-
 ProbRed_Yes = ProbYesRed*ProbRed/ProbYes
 
 #Calculated Probability of Stolen=Yes and Tpe=SUV
 ProbYesSuv = len([row for row in data if row[2]=='Suv' and row[4]=='Yes'])/len([row for row in data if row[2]=='Suv'])
-ProbSuv = len([row for row in data if row[2]=='Suv'])/len(data)
-
 ProbSuv_Yes = ProbYesSuv*ProbSuv/ProbYes
 
 
 #Calculated Probability of Stolen=Yes and Origin=Domestic
 ProbDomesYes = len([row for row in data if row[3]=='Domestic' and row[4]=='Yes'])/len([row for row in data if row[3]=='Domestic'])
-ProbDomes = len([row for row in data if row[3]=='Domestic'])/len(data)
-
 ProbDom_Yes = ProbDomesYes*ProbDomes/ProbYes
 
 #Calculated Probability of Stolen=Yes Over X = {(Color="Red", Type="SUV"; and Origin="Domestic") }
